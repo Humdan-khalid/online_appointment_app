e8349d0aea97680c6b9d29ff12ed462b01c2622c
Hamdan Khalid <hamdankhalid663@gmail.com>
2026-01-04 15:40:03 +0500

    I create the two functions one is create the hash password and second is verify the hash password function.

---
+++ password_hashing.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+    bcrypt__rounds=10
+)
+
+def create_hash_password(password: str):
+    if password is None:
+        raise ValueError("Password is None!")
+    password_hashing = pwd_context.hash(password)
+    print(password_hashing)
+    return password_hashing
+
+
+def verify_hasing_password(plain_password: str, hash_password: str):
+    if plain_password is None or hash_password is None:
+        raise ValueError("plain password or hash password is None")
+    password_verify = pwd_context.verify(plain_password, hash_password)
+    return password_verify
+

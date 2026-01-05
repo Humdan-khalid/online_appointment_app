7dc69b797689e4c329820f238c6459c3a0aed5b6
Hamdan Khalid <hamdankhalid663@gmail.com>
2026-01-05 15:51:09 +0500

    create the new Staffs table in database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field
 from pydantic import EmailStr, BaseModel
 from datetime import datetime
 
+
 class Users(SQLModel, table = True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(min_length=3, max_length=40, nullable=False)
@@ -20,5 +21,26 @@ class CreateUser(BaseModel):
     password: str = Field(min_length=8, max_length=100)
 
 class VerifyUser(SQLModel, BaseModel):
+    email: EmailStr
+    password: str
+
+class Staffs(SQLModel, table = True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(min_length=3, max_length=30, nullable=False)
+    age: int = Field(ge=18, le=75)
+    profession: str = Field(nullable=False)
+    phone_number: str = Field(min_length=11, max_length=11, nullable=False, unique=True)
+    email: EmailStr = Field(nullable=False, index=True)
+    password: str = Field(min_length=8)
+
+class CreateStaff(BaseModel):
+    name: str = Field(min_length=3, max_length=30, nullable=False)
+    age: int = Field(ge=18, le=75)
+    profession: str = Field(nullable=False)
+    phone_number: str = Field(min_length=11, max_length=11, nullable=False, unique=True)
+    email: EmailStr = Field(nullable=False, index=True)
+    password: str = Field(min_length=8)
+
+class VerifyStaff(BaseModel):
     email: EmailStr
     password: str

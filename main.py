36f8483aa9ae72b256a15a9a68925e0f279ca4b4
Hamdan Khalid <hamdankhalid663@gmail.com>
2026-01-05 17:21:32 +0500

    I create the new 2 APIs first is staff signup API and second is staff login API.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlmodel import Session, select
 from database_connection import engine
-from database import Users, CreateUser, VerifyUser
+from database import Users, CreateUser, VerifyUser, CreateStaff, Staffs, VerifyStaff
 from password_hashing import create_hash_password, verify_hash_password
 from jwt_tokens import create_token, verify_token
 
@@ -51,3 +51,47 @@ def user_login(user: VerifyUser, session: Session=Depends(get_session)):
     )
     return{"access_token": payload,
            "token_type": "Bearer"}
+
+@app.post("/staff", status_code=status.HTTP_201_CREATED)
+def create_staff(staff: CreateStaff, session: Session=Depends(get_session)):
+    db_staff=session.exec(select(Staffs).where(Staffs.email == staff.email)).first()
+    
+    if db_staff:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="already account exist at this email.")
+    
+    new_staff=Staffs(
+        name=staff.name,
+        profession=staff.profession,
+        age=staff.age,
+        phone_number=staff.phone_number,
+        email=staff.email,
+        password=create_hash_password(staff.password)        
+    )
+
+    session.add(new_staff)
+    session.commit()
+
+    return{
+        "id": new_staff.id,
+        "name": new_staff.name,
+        "profession": new_staff.profession
+        }
+
+@app.post("/login", status_code=status.HTTP_200_OK)
+def staff_login(staff: VerifyStaff, session: Session=Depends(get_session)):
+    db_staff = session.exec(select(Staffs).where(Staffs.email == staff.email)).first()
+    
+    if not db_staff:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password!")
+
+    if not verify_hash_password(staff.password, db_staff.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password!")
+    
+    payload=create_token(
+        {"id": db_staff.id,
+         "phone_number": db_staff.phone_number,
+         "email": db_staff.email}
+    )
+    return{"access_token": payload,
+           "token_type": "Bearer"}
+     
